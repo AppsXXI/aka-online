6c7d5b4036246ed5a6b424abd754c5dc8252f454
Miguelslo27 <miguel.sosa@globant.com>
2018-08-04 22:47:18 -0300

    feature/courses-service-n-mocks - Added first basic courses service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { ClarityModule, ClrFormsNextModule } from "@clr/angular";
 import { AppRoutingModule } from './app-routing.module';
 
 // COMPONENTS
+// COMMONS
 import { HTitleComponent } from './commons/components/basics/h-title/h-title.component';
-import { Aka404NotFoundComponent } from './components/aka-404-not-found/aka-404-not-found.component';
-import { AppComponent } from './app.component';
 import { AkaNavigationComponent } from './commons/components/aka-navigation/aka-navigation.component';
 import { AkaLoginComponent } from './commons/components/aka-login/aka-login.component';
-import { AkaLoginScreenComponent } from './components/aka-login-screen/aka-login-screen.component';
 import { AkaModalComponent } from './commons/components/aka-modal/aka-modal.component';
 import { AkaModalsComponent } from './commons/components/aka-modals/aka-modals.component.';
 import { AkaCourseCardComponent } from './commons/components/aka-course-card/aka-course-card.component';
+// SCREENS
+import { Aka404NotFoundComponent } from './components/aka-404-not-found/aka-404-not-found.component';
+import { AppComponent } from './app.component';
+import { AkaLoginScreenComponent } from './components/aka-login-screen/aka-login-screen.component';
 import { AkaMyCoursesComponent } from './components/aka-my-courses/aka-my-courses.component';
 import { AkaActiveCoursesComponent } from './components/aka-my-courses/aka-active-courses/aka-active-courses.component';
 import { AkaApprovedCoursesComponent } from './components/aka-my-courses/aka-approved-courses/aka-approved-courses.component';
@@ -22,6 +24,7 @@ import { AkaPathComponent } from './components/aka-my-courses/aka-path-courses/a
 
 // SERVICES
 import { AkaBasicsService } from './commons/services/aka-basics.service';
+import { AkaCoursesService } from './commons/services/aka-courses.service';
 
 @NgModule({
   declarations: [
@@ -48,7 +51,8 @@ import { AkaBasicsService } from './commons/services/aka-basics.service';
     AppRoutingModule
   ],
   providers: [
-    AkaBasicsService
+    AkaBasicsService,
+    AkaCoursesService
   ],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]

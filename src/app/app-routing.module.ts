4383e83d7e62d2748ba66baa03a0f2169c707295
Miguelslo27 <miguel.sosa@globant.com>
2018-08-04 21:19:07 -0300

    feature/improving-routing - Added child routing for my courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AkaLoginScreenComponent } from './components/aka-login-screen/aka-login-screen.component';
 import { AkaMyCoursesComponent } from './components/aka-my-courses/aka-my-courses.component';
+import { AkaActiveCourses } from './components/aka-my-courses/aka-active-courses/aka-active-courses.component';
+import { AkaApprovedCourses } from './components/aka-my-courses/aka-approved-courses/aka-approved-courses.component';
+import { AkaPathComponent } from './components/aka-my-courses/aka-path-courses/aka-path-courses.component';
 
 const ROUTES: Routes = [
   {
@@ -16,7 +19,21 @@ const ROUTES: Routes = [
   },
   {
     path: 'my-courses',
-    component: AkaMyCoursesComponent
+    component: AkaMyCoursesComponent,
+    children: [
+      {
+        path: 'active',
+        component: AkaActiveCourses
+      },
+      {
+        path: 'path',
+        component: AkaPathComponent
+      },
+      {
+        path: 'approved',
+        component: AkaApprovedCourses
+      }
+    ]
   }
 ];
 
---
+++ src/app/components/aka-my-courses/aka-path-courses/aka-path-courses.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'aka-path-courses',
+  templateUrl: './aka-path-courses.component.html',
+  styleUrls: ['./aka-path-courses.component.scss']
+})
+export class AkaPathComponent {}

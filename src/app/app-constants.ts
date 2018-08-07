ac9a4793ea94e1de5ef3c0733dacfebe8c6623f3
Miguelslo27 <miguel.sosa@globant.com>
2018-08-07 10:45:39 -0300

    feature/improving-routing - Improved routes to match CONSTANTS

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -8,26 +8,26 @@ export const CONSTANTS = {
       URL: 'my-courses',
       ACTIVE: {
         URL: 'active',
-        FULL_URL: `my-courses/active`
+        FULL_URL: 'my-courses/active'
       },
       PATH: {
         URL: 'path',
-        FULL_URL: `my-courses/path`
+        FULL_URL: 'my-courses/path'
       },
       APPROVED: {
         URL: 'approved',
-        FULL_URL: `my-courses/approved`
+        FULL_URL: 'my-courses/approved'
       }
     },
     EXPLORE: {
       URL: 'explore',
       CATEGORY: {
         URL: 'category',
-        FULL_URL: `explore/category`
+        FULL_URL: 'explore/category'
       },
       ALPHABET: {
         URL: 'alphabet',
-        FULL_URL: `explore/alphabet`
+        FULL_URL: 'explore/alphabet'
       }
     }
   },

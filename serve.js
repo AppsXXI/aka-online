e7df67481273d7f1f986b4387e4058ec84f50136
Miguelslo27 <miguel.sosa@globant.com>
2018-11-17 11:41:51 -0300

    feature/deployment-config - Added server port variable to run propertly

--- serve.js
+++ serve.js
@@ -4,6 +4,7 @@ const app = express();
 const angularConfig = require('./angular');
 const packageConfig = require('./package');
 
+const port = process.env.PORT || 8080;
 const appName = packageConfig.name;
 const appDir = angularConfig.projects[packageConfig.name].architect.build.options.outputPath;
 
@@ -13,10 +14,10 @@ console.log(appDir);
 app.use(express.static(`${__dirname}/${appDir}`));
 
 app.get('/*', (req, res) => {
-  // res.send('Hola mundo!');
   res.sendFile(path.join(`${__dirname}/${appDir}/index.html`));
 });
 
-app.listen(8080, () => {
-  console.log("Server online");
+console.log("Server is starting...");
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
